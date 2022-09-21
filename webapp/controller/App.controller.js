b6679dd407bdf1f85e10868ee3190b9c00e095b0
KCsenge <csengekormoczy@gmail.com>
2022-09-21 17:10:04 +0200

    JSON Model

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,8 +1,24 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
-  "use strict";
-  return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-    onShowHello: function () {
-      alert("Hello World");
-    },
-  });
-});
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+  ],
+  function (Controller, MessageToast, JSONModel) {
+    "use strict";
+    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+      onInit: function () {
+        var oData = {
+          recipent: {
+            name: "World",
+          },
+        };
+        var oModel = new JSONModel(oData);
+        this.getView().setModel(oModel);
+      },
+      onShowHello: function () {
+        MessageToast.show("Hello World");
+      },
+    });
+  }
+);
