fd16d75f08cb6f3fc691a0c425811798cb779dd3
KCsenge <csengekormoczy@gmail.com>
2022-09-21 12:54:37 +0200

    Bootstrap

---
+++ dist/Component-preload.js
@@ -0,0 +1,6 @@
+//@ui5-bundle sap/ui/demo/walkthrough/Component-preload.js
+sap.ui.require.preload({
+	"sap/ui/demo/walkthrough/index.js":function(){sap.ui5.define([],function(){"use strict";alert("UI5 is ready")});
+},
+	"sap/ui/demo/walkthrough/manifest.json":'{"sap.app":{"id":"sap.ui.demo.walkthrough","type":"application"}}'
+});
---
+++ dist/index.js
@@ -0,0 +1,1 @@
+sap.ui5.define([],function(){"use strict";alert("UI5 is ready")});
